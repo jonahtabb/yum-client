136d5ccb39af6c75da471100d5d2c6e32c67d0cd
jonahtabb <jonahtabb@gmail.com>
2021-08-04 20:21:42 -0400

    updated environment file to run correctly locally

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,7 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3001';
+    APIURL = 'http://localhost:3000';
     break;
     case 'efa-yumapp.herokuapp.com':
     APIURL = 'https://jbl-yum-server.herokuapp.com'
