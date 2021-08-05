8a648e031eb0242f2ff50d70a7a4f82f37be90a7
jonahtabb <jonahtabb@gmail.com>
2021-08-04 20:21:42 -0400

    updated environment file to run correctly locally

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -2,7 +2,7 @@ let APIURL = '';
 
 switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3001';
+    APIURL = 'http://localhost:3000';
     break;
     case 'jtabb-yum-app.herokuapp.com':
     APIURL = 'https://jtabb-yum-app-server.herokuapp.com'
