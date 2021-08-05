bd0147d20776ab34191fcf7b08231557b86f5ed2
jonahtabb <jonahtabb@gmail.com>
2021-08-05 18:03:41 -0400

    initial commit of yum app client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Portal from "./components/Portal";
-
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './components/ui/theme'
 
 
 function App() {
   return (
-    <div className="App">
-      <Portal />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Portal />
+      </div>
+    </ThemeProvider>
+
   );
 }
 
